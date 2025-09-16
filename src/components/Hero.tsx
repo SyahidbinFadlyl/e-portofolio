c96290622f3f2f7cd66d7f69102d5a5ed1b3a6c3
muhammad.syahid <ava.rageman18@gmail.com>
2025-09-16 14:40:03 +0700

    Remove buttons from Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,19 +12,11 @@ export default function Hero() {
       }}
     >
       <Typography variant="h2" gutterBottom>
-        Hi, I’m Syahid 👋
+        Hi, I’m Syahid
       </Typography>
       <Typography variant="h5" color="text.secondary" gutterBottom>
         Software Engineer | Web Developer | Problem Solver
       </Typography>
-      <Box mt={3}>
-        <Button variant="contained" color="primary" sx={{ mr: 2 }}>
-          View Projects
-        </Button>
-        <Button variant="outlined" color="secondary">
-          Contact Me
-        </Button>
-      </Box>
     </Container>
   );
 }
