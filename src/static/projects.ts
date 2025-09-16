351e31e2723a80fd06ad97d459e0ddb7a130b498
muhammad.syahid <ava.rageman18@gmail.com>
2025-09-16 19:06:56 +0700

    Add images for BPRS and RQ landing page projects

--- src/static/projects.ts
+++ src/static/projects.ts
@@ -1,7 +1,17 @@
 import { Project } from "@/components/ProjectCard"
 import crm from "../../public/crm.png"
+import bprs from "../../public/bprs.png"
+import rq_landing from "../../public/rq_landing.png"
 
 const project_list: Project[] = [
+    {
+        image: rq_landing,
+        title: "Web App RQ Daarut Tarbiyah",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, officia
+        deleniti quasi perferendis expedita fugiat placeat eius. Praesentium,
+        magnam. Obcaecati fuga et soluta unde quas aliquid incidunt rerum
+        perferendis perspiciatis!`,
+    },
     {
         image: crm,
         title: "CRM Home Buyer",
@@ -9,7 +19,15 @@ const project_list: Project[] = [
         deleniti quasi perferendis expedita fugiat placeat eius. Praesentium,
         magnam. Obcaecati fuga et soluta unde quas aliquid incidunt rerum
         perferendis perspiciatis!`,
-    }
+    },
+    {
+        image: bprs,
+        title: "CRM BPRS",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, officia
+        deleniti quasi perferendis expedita fugiat placeat eius. Praesentium,
+        magnam. Obcaecati fuga et soluta unde quas aliquid incidunt rerum
+        perferendis perspiciatis!`,
+    },
 ]
 
 export default project_list
