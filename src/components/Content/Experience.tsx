c5fff675b12b53b2ae441e85cea84a604fa0013f
muhammad.syahid <ava.rageman18@gmail.com>
2025-09-16 17:48:58 +0700

    Add IconButton for viewing full resume in Experience component

--- src/components/Content/Experience.tsx
+++ src/components/Content/Experience.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography, IconButton } from "@mui/material";
 import ExperienceCard, { Experience } from "../ExperienceCard";
 import list_experience from "@/static/experience";
 
@@ -8,11 +8,27 @@ export default function Experience() {
   return (
     <Box>
       {/* <Typography sx={{ fontWeight: 700, mb: 4 }}>Experience</Typography> */}
-      <Box display={"flex"} flexDirection={"column"} gap={"2rem"}>
+      <Box display={"flex"} flexDirection={"column"} gap={"2rem"} pb={"2rem"}>
         {experiences.map((exp, idx) => (
           <ExperienceCard key={idx} experience={exp} />
         ))}
       </Box>
+      <IconButton
+        component="a"
+        href="https://github.com/SyahidbinFadlyl"
+        target="_blank"
+        rel="noopener"
+        color="inherit"
+        sx={{
+          fontSize: "1rem",
+          fontWeight: 700,
+          "&:hover": {
+            color: "rgba(94, 234, 212, 1) !important",
+          },
+        }}
+      >
+        View Full Resume
+      </IconButton>
     </Box>
   );
 }
