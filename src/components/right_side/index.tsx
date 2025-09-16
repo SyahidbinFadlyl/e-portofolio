378078081b907c1ff3d8ef94960794b43e30f203
muhammad.syahid <ava.rageman18@gmail.com>
2025-09-16 16:00:11 +0700

    Update right side component layout and social icons

--- src/components/right_side/index.tsx
+++ src/components/right_side/index.tsx
@@ -1,27 +1,71 @@
-import { Box, Typography } from "@mui/material";
+import { Box, IconButton, Stack, Typography } from "@mui/material";
+import { LinkedIn, GitHub } from "@mui/icons-material";
 
 export default function LeftSide() {
   return (
     <Box
       sx={{
         py: { sm: "6rem", xs: "3rem" },
+        height: { sm: "100vh" },
+        "@media (min-width:1200px)": {
+          position: "sticky",
+          top: 0,
+          display: "flex",
+          maxHeight: "100vh",
+          width: "48%",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        },
       }}
     >
-      <Typography
-        sx={{ fontSize: { sm: "3rem", xs: "2rem" }, fontWeight: 700 }}
-      >
-        Muhammad Syahid
-      </Typography>
-      <Typography
+      <Box>
+        <Typography
+          sx={{ fontSize: { sm: "3rem", xs: "2rem" }, fontWeight: 700 }}
+        >
+          Muhammad Syahid
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: { sm: "1.25rem", xs: "1.125rem" },
+            fontWeight: 500,
+            lineHeight: { sm: "1.75rem" },
+            mt: "0.75rem",
+          }}
+        >
+          Software Engineer
+        </Typography>
+        <Typography sx={{ mt: "1rem", fontSize: "1rem" }}>
+          I build custom web applications
+        </Typography>
+      </Box>
+
+      <Box
         sx={{
-          fontSize: { sm: "1.25rem", xs: "1.125rem" },
-          fontWeight: 500,
-          lineHeight: { sm: "1.75rem" },
-          mt: "0.75rem",
+          ml: "0.25rem",
+          mt: "2rem",
         }}
       >
-        Software Engineer
-      </Typography>
+        <Stack direction="row" spacing={0}>
+          <IconButton
+            component="a"
+            href=""
+            target="_blank"
+            rel="noopener"
+            color="inherit"
+          >
+            <GitHub />
+          </IconButton>
+          <IconButton
+            component="a"
+            href=""
+            target="_blank"
+            rel="noopener"
+            color="inherit"
+          >
+            <LinkedIn />
+          </IconButton>
+        </Stack>
+      </Box>
     </Box>
   );
 }
