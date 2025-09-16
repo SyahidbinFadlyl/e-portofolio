b36e8029b6fb5eb08b7a94ab9c1d153ec46f72ac
muhammad.syahid <ava.rageman18@gmail.com>
2025-09-16 18:34:09 +0700

    Add underline style to the Hacktiv8 link

--- src/components/Content/About.tsx
+++ src/components/Content/About.tsx
@@ -14,7 +14,7 @@ export default function About() {
     >
       <Typography>
         I am an inquisitive person, minimalist, committed, and reliable in many
-        ways. I participated in
+        ways. I participated in{" "}
         <span
           onClick={() => {
             window?.open("https://www.hacktiv8.com/");
@@ -23,10 +23,11 @@ export default function About() {
             color: "#e2e8f0",
             fontWeight: 500,
             cursor: "pointer",
+            textDecoration: "underline",
           }}
         >
-          {` Hacktiv8's `}
-        </span>
+          {`Hacktiv8's`}
+        </span>{" "}
         Full Stack Javascript Immersive bootcamp, which allowed me to become
         experienced with Javascript and currently popular technology stacks.
       </Typography>
