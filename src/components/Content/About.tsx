3750d3bb69899a363f8f5ae7254747b723629f5f
muhammad.syahid <ava.rageman18@gmail.com>
2025-09-16 16:22:34 +0700

    Update About component content with personal information

--- src/components/Content/About.tsx
+++ src/components/Content/About.tsx
@@ -10,10 +10,10 @@ export default function About() {
       color={"#94A3B8"}
     >
       <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, officia
-        deleniti quasi perferendis expedita fugiat placeat eius. Praesentium,
-        magnam. Obcaecati fuga et soluta unde quas aliquid incidunt rerum
-        perferendis perspiciatis!
+        I am an inquisitive person, minimalist, committed, and reliable in many
+        ways. I participated in Hacktiv8's Full Stack Javascript Immersive
+        bootcamp, which allowed me to become experienced with Javascript and
+        currently popular technology stacks.
       </Typography>
       <Typography>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, officia
