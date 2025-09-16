431c2991561eaf1f48b9d90e54eefcff8c471537
muhammad.syahid <ava.rageman18@gmail.com>
2025-09-16 19:17:24 +0700

    Update project title in project list to shorten it

--- src/static/projects.ts
+++ src/static/projects.ts
@@ -7,7 +7,7 @@ import task_manager from "../../public/task_manager.png"
 const project_list: Project[] = [
     {
         image: rq_landing,
-        title: "Web App RQ Daarut Tarbiyah",
+        title: "RQ Daarut Tarbiyah",
         description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, officia
         deleniti quasi perferendis expedita fugiat placeat eius. Praesentium,
         magnam. Obcaecati fuga et soluta unde quas aliquid incidunt rerum
