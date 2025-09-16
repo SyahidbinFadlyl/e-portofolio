befeb05ccf20c97f0deb8997243620ba7e5383b5
muhammad.syahid <ava.rageman18@gmail.com>
2025-09-16 17:02:00 +0700

    Add list of experiences from separate file

--- src/components/Content/Experience.tsx
+++ src/components/Content/Experience.tsx
@@ -1,34 +1,10 @@
 import React from "react";
-import { Box, Container, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import ExperienceCard, { Experience } from "../ExperienceCard";
+import list_experience from "@/static/experience";
 
 export default function Experience() {
-  const experiences: Experience[] = [
-    {
-      year: "2024 — Present",
-      title: "Full Stack Developer",
-      company: "Amani",
-      description:
-        "Build and maintain critical components used to construct frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.",
-      skills: ["JavaScript", "TypeScript", "React", "NextJs", "Node.js"],
-    },
-    {
-      year: "2022 — 2024",
-      title: "Full Stack Developer",
-      company: "Happy Homes",
-      description:
-        "Build and maintain critical components used to construct frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.",
-      skills: [
-        "JavaScript",
-        "TypeScript",
-        "HTML & SCSS",
-        "React",
-        "Next.js",
-        "React Native",
-        "Node.js",
-      ],
-    },
-  ];
+  const experiences: Experience[] = list_experience;
   return (
     <Box>
       {/* <Typography sx={{ fontWeight: 700, mb: 4 }}>Experience</Typography> */}
---
+++ src/static/experience.ts
@@ -0,0 +1,29 @@
+import { Experience } from "@/components/ExperienceCard"
+
+const list_experience: Experience[] = [
+    {
+        year: "2024 — Present",
+        title: "Full Stack Developer",
+        company: "Amani",
+        description:
+            "Build and maintain critical components used to construct frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.",
+        skills: ["JavaScript", "TypeScript", "React", "NextJs", "Node.js"],
+    },
+    {
+        year: "2022 — 2024",
+        title: "Full Stack Developer",
+        company: "Happy Homes",
+        description:
+            "Build and maintain critical components used to construct frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.",
+        skills: [
+            "JavaScript",
+            "TypeScript",
+            "HTML & SCSS",
+            "React",
+            "Next.js",
+            "React Native",
+            "Node.js",
+        ],
+    },
+];
+export default list_experience
