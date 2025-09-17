749875bb296c17f348f203d80870149a225dfbbb
Muhammad Syahid <muhammad.syahid@amani.co.id>
2025-09-17 11:48:18 +0700

    delete unused code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,37 +9,6 @@ const inter = Inter({
   variable: "--font-inter",
 });
 
-// const theme = createTheme({
-//   palette: {
-//     mode: "light",
-//     primary: {
-//       main: "#e2e8f0",
-//     },
-//     secondary: {
-//       main: "#94A3B8",
-//     },
-//     background: {
-//       default: "rgba(15, 23, 42, 1)",
-//     },
-//     text: {
-//       primary: "#e2e8f0",
-//       secondary: "#94A3B8",
-//     },
-//   },
-//   typography: {
-//     fontFamily: "Inter",
-//   },
-//   components: {
-//     MuiCssBaseline: {
-//       styleOverrides: {
-//         html: {
-//           scrollBehavior: "smooth",
-//         },
-//       },
-//     },
-//   },
-// });
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <html lang="en" className={inter.variable}>
