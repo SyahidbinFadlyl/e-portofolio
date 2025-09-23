8526095520bbb07b0f2e9b90238136d73b448e54
Muhammad Syahid <muhammad.syahid@amani.co.id>
2025-09-23 13:24:15 +0700

    Update experience descriptions for Software Engineer and Full Stack Developer

--- src/static/experience.ts
+++ src/static/experience.ts
@@ -5,24 +5,19 @@ const list_experience: Experience[] = [
         year: "Okt 2024 — Present",
         title: "Software Engineer",
         company: "Amani",
-        description: `Build and maintain internal CRM applications (front end & back end).\nCollaborate with system analysts, designers, and project managers to implement best practices in accessibility and functionality.\nSupport core features including property sales management, customer data processing, financial reporting, and payment tracking for mortgage and cash transactions.`,
+        description: `Build and maintain internal CRM applications (front end & back end).\n\nCollaborate with system analysts, designers, and project managers to implement best practices in accessibility and functionality.\n\nSupport core features including property sales management, customer data processing, financial reporting, and payment tracking for mortgage and cash transactions.`,
         skills: ["JavaScript", "TypeScript", "React", "NextJs", "Node.js"],
     },
     {
         year: "Okt 2022 — Okt 2024",
         title: "Full Stack Developer",
         company: "Happy Homes",
-        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, officia
-        deleniti quasi perferendis expedita fugiat placeat eius. Praesentium,
-        magnam. Obcaecati fuga et soluta unde quas aliquid incidunt rerum
-        perferendis perspiciatis!`,
+        description: `Build and maintain internal CRM applications (front end & back end).\n\nCollaborate with designers, and project managers to implement best practices in accessibility and functionality.\n\nActively handle challenges in adapting application flows and features from legacy business processes to new ones, ensuring that changes do not create issues with existing data.`,
         skills: [
             "JavaScript",
             "TypeScript",
-            "HTML & SCSS",
             "React",
             "Next.js",
-            "React Native",
             "Node.js",
         ],
     },
